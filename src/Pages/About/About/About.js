4bd50b2b7ad51a97b93fd2d51c3395daba5da623
JobaerSiddique <jobaersiddique28me@gmail.com>
2023-02-25 12:55:03 +0600

    solve about

--- src/Pages/About/About/About.js
+++ src/Pages/About/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
   <div className="hero-content flex-col lg:flex-row-reverse">
     <img src={img} alt="" className='w-2/4' />
     <div>
-      <h1 className="text-xl text-justify font-bold p-5">Before start welcome to Safir Bike Point and in this page you will get All bike price in bd and many helpful blogs check full page thanks. Motorcycle is one of the most popular vehicles in Bangladesh and all over the world. On this page, you will get updated Bangladeshi Motorcycles price list with review and latest motorbike & scooter price updates.
+      <h1 className=" text-justify font-bold p-5 text-base lg:text-xl">Before start welcome to Safir Bike Point and in this page you will get All bike price in bd and many helpful blogs check full page thanks. Motorcycle is one of the most popular vehicles in Bangladesh and all over the world. On this page, you will get updated Bangladeshi Motorcycles price list with review and latest motorbike & scooter price updates.
 
 Motorcycle / Motorbike & Scooter, Whatever you call it, You will get all available bike brands in BD showrooms & Specifications with image details from every bike page.
 
